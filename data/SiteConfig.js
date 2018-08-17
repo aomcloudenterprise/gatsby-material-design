9b4b929c658a910e7df2ec5dd26ad4c9e51a236d
danklut <aomcloudenterprise@outlook.com>
2018-08-17 05:27:25 -0700

    Update SiteConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -9,9 +9,9 @@ module.exports = {
   siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  siteGATrackingID: "UA-47311644-4", // Tracking code ID for google analytics.
+  siteGATrackingID: "UA-115066134-2", // Tracking code ID for google analytics.
   disqusShortname: "https-aomcloudenterprise-github-io-gatsby-material-starter", // Disqus shortname.
-  postDefaultCategoryID: "Tech", // Default category for posts.
+  postDefaultCategoryID: "Narketing", // Default category for posts.
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "DD/MM/YYYY", // Date format for display.
   userName: "danklut", // Username to display in the author segment.
