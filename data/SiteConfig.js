153d2528a2af347e71320a00b201cfd7ade498c7
danklut <aomcloudenterprise@outlook.com>
2018-08-19 09:43:41 -0700

    Update SiteConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,10 +3,10 @@ module.exports = {
   siteTitleShort: "GM Starter", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "GatsbyJS Material Starter", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://github.com/aomcloudenterprise/gatsby-material-starter," // Domain of your website without pathPrefix.
-  pathPrefix: "/gatsby-material-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  siteUrl: "https://github.com/aomcloudenterprise/gatsby-material-design," // Domain of your website without pathPrefix.
+  pathPrefix: "/gatsby-material-design", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
-  siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "A GatsbyJS starter with Material design in mind.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   siteGATrackingID: "UA-115066134-2", // Tracking code ID for google analytics.
@@ -24,7 +24,7 @@ module.exports = {
   userLinks: [
     {
       label: "GitHub",
-      url: "https://github.com/aomcloudenterprise/gatsby-material-starter",
+      url: "https://github.com/aomcloudenterprise/gatsby-material-design",
       iconClassName: "fa fa-github"
     },
     {
