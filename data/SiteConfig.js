2ccc96e53cb8148205458b12bbf8e0cebe71415b
danklut <aomcloudenterprise@outlook.com>
2018-08-20 16:15:27 -0700

    Update SiteConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,8 +3,8 @@ module.exports = {
   siteTitleShort: "GM Starter", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "GatsbyJS Material Starter", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://github.com/aomcloudenterprise/gatsby-material-starter," // Domain of your website without pathPrefix.
-  pathPrefix: "/gatsby-material-starter"// Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  siteUrl: "https://github.com/aomcloudenterprise/gatsby-material-starter.git," // Domain of your website without pathPrefix.
+  pathPrefix: "/gatsby-material-starter",// Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription: "A GatsbyJS starter with Material design in mind.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
